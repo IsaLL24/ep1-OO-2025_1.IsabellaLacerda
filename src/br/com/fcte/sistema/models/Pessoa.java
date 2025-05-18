b7242dafb02d663a480cb116d6e843890a716b0f
Isabella Lacerda Lima <isalima2004@gmail.com>
2025-05-18 20:17:24 -0300

    Update Pessoa.java
    -A classe Pessoa é uma classe base no sistema acadêmico que implementa o conceito de encapsulamento.
    - .models, que é onde estão as classes de modelo do sistema.
    -(private String nome;
    private String id;)
     private implementa o encapsulamento, impedindo acesso direto a esses atributos de fora da classe.
    -Os métodos getNome() e getId() (getters) permitem ler os valores dos atributos.
    -Os métodos setNome() e setId() (setters) permitem modificar os valores dos atributos.
    -Método toString(): Ele retorna uma representação em string da instância da classe, mostrando o ID e o nome.
    É útil para depuração e exibição de informações sobre a pessoa.

--- src/br/com/fcte/sistema/models/Pessoa.java
+++ src/br/com/fcte/sistema/models/Pessoa.java
@@ -3,50 +3,37 @@ package br.com.fcte.sistema.models;
 public class Pessoa {
 	private String nome;
     private String id;
-    
-    /**
-     * Inicializa uma nova instância da classe Pessoa.
-     * 
-     * @param nome Nome da pessoa
-     * @param id Identificador único da pessoa
-     */
+
+     // Inicializa uma nova instância da classe Pessoa.
+     
+     // nome Nome da pessoa
+     //id Identificador único da pessoa
+     
     public Pessoa(String nome, String id) {
         this.nome = nome;
         this.id = id;
     }
     
-    /**
-     * Retorna o nome da pessoa.
-     * 
-     * @return Nome da pessoa
-     */
+    // Retorna o nome da pessoa.
+    
     public String getNome() {
         return nome;
     }
-    
-    /**
-     * Define o nome da pessoa.
-     * 
-     * @param nome Novo nome da pessoa
-     */
+    // Define o nome da pessoa.
+    // nome Novo nome da pessoa
+
     public void setNome(String nome) {
         this.nome = nome;
     }
     
-    /**
-     * Retorna o identificador único da pessoa.
-     * 
-     * @return Identificador único da pessoa
-     */
+    //Retorna o identificador único da pessoa.
+    
     public String getId() {
         return id;
     }
     
-    /**
-     * Define o identificador único da pessoa.
-     * 
-     * @param id Novo identificador único da pessoa
-     */
+    // Define o identificador único da pessoa.
+     
     public void setId(String id) {
         this.id = id;
     }
