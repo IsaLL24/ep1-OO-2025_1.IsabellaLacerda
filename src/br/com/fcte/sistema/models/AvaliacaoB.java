506468feada19536a62b98f4357b52100afb4d14
Isabella Lacerda Lima <isalima2004@gmail.com>
2025-05-24 15:39:28 -0300

    Update AvaliacaoB.java
    -Classe concreta, Implementa outra forma específica de calcular a média final, diferente da AvaliacaoA, mas ainda seguindo o contrato da interface Avaliacao.
    Representa a avaliação do "Tipo B".
    
    -Implementação da Interface Avaliacao:
    Declara implements Avaliacao, obrigando-se a fornecer os métodos getTipo() e calcularMedia().
    
    -Método getTipo():
    @Override indica que está implementando o método da interface.
    Retorna a String "B" para identificar este tipo de avaliação.
    
    -Método calcularMedia(Map<String, Double> notas):
    @Override indica que está implementando o método da interface.
    Validação: Similar à AvaliacaoA, verifica se o mapa notas contém todas as chaves necessárias ("P1", "P2", "P3", "L", "S"). Retorna -1 se alguma estiver faltando.
    Cálculo: Se todas as notas estiverem presentes, calcula a média ponderada: P1 tem peso 1, P2 tem peso 2, P3 tem peso 3, L tem peso 1 e S tem peso 1. A soma dos produtos (nota * peso) é dividida pela soma dos pesos (8.0).
    Fórmula: (P11 + P22 + P33 + L1 + S*1) / 8
    
    -Método toString():
    @Override indica que está sobrescrevendo o método toString da classe Object.
    Retorna uma representação textual da avaliação, incluindo o tipo ("B") e a fórmula ponderada utilizada.

--- src/br/com/fcte/sistema/models/AvaliacaoB.java
+++ src/br/com/fcte/sistema/models/AvaliacaoB.java
@@ -24,7 +24,7 @@ public class AvaliacaoB implements Avaliacao {
             return -1;
         }
         
-        // Calcular a média
+        // Calcular a média.
         return (notas.get("P1") + notas.get("P2") * 2 + notas.get("P3") * 3 + 
                 notas.get("L") + notas.get("S")) / 8.0;
     }
